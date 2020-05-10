c5dcee033d9c2e37af6cfb48b59e8cd8b437c42f
by-student-2017 <studentsctest@gmail.com>
2020-05-10 20:37:59 +0900

    add hydride

---
+++ glass/SiO2/80/run.bat
@@ -0,0 +1,3 @@
+mkdir cfg
+
+"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in SiO2_N80.in
---
+++ glass/TiO2/80/run.bat
@@ -0,0 +1,3 @@
+mkdir cfg
+
+"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in TiO2_N80.in
